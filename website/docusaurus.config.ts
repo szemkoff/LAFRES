492857940323ae001e131996b149a8e98652e6e5
Stan Zemskoff <stantheman@gmail.com>
2025-12-05 13:53:05 -0500

    Add custom L.A.F.R.E.S. social card for link previews

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -71,8 +71,8 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    // Social card for link previews
+    image: 'img/lafres-social-card.svg',
     colorMode: {
       respectPrefersColorScheme: true,
     },
